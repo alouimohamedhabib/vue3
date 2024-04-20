8a6b561b4064c9877805c8d4fdda9b52798b0de8
Mohamed Habib ALOUI <alouimohamedhabib@yahoo.com>
2024-04-21 00:03:19 +0200

    fix props mutation on taskEdit component

--- src/components/Tasks/TaskEdit.vue
+++ src/components/Tasks/TaskEdit.vue
@@ -1,17 +1,31 @@
 <template>
-  <div class="fixed h-full w-full bg-black bg-opacity-80 top-0 left-0 flex justify-center items-center" v-if="task">
-    <div class="todo bg-white p-4 rounded  w-4/12 h-40/6">
+  <div
+    class="fixed h-full w-full bg-black bg-opacity-80 top-0 left-0 flex justify-center items-center"
+    v-if="task"
+  >
+    <div class="todo bg-white p-4 rounded w-4/12 h-40/6">
       <div class="mb-6">
-        <label for="large-input" class="block mb-2 text-sm font-medium text-gray-900 ">Todo content</label>
-        <input type="text" id="large-input" v-model="props.task.todo"
-               class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <label for="large-input" class="block mb-2 text-sm font-medium text-gray-900"
+          >Todo content</label
+        >
+        <input
+          type="text"
+          id="large-input"
+          v-model="todoCopy.todo"
+          class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
       </div>
-      <button type="submit"
-             :onclick="handleFormSubmission"
-              class="text-white  end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+      <div class="mb-6">
+        <label for="" class="block mb-2 text-sm font-medium text-gray-900">Done or not</label>
+        <input type="checkbox" :checked="task.completed" v-model="todoCopy.completed" />
+      </div>
+      <button
+        type="submit"
+        :onclick="handleFormSubmission"
+        class="text-white end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
         Save
       </button>
-
     </div>
   </div>
   <div class="div" v-else>Please select a task to edit</div>
@@ -24,12 +38,12 @@ const emit = defineEmits(['updateTask'])
 // const t = ref("12")
 const props = defineProps({
   task: {
-    type: Object as PropType<TaskObject>, default: null
+    type: Object as PropType<TaskObject>,
+    default: null
   }
 })
-const todoCopy = ref(props.task )
+const todoCopy = ref(props.task)
 const handleFormSubmission = () => {
-  emit('updateTask', {...todoCopy.value})
+  emit('updateTask', { ...todoCopy.value })
 }
-
 </script>
--- src/components/Tasks/TaskItem.vue
+++ src/components/Tasks/TaskItem.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="task-item flex p-4 bg-white mt-2 mb-2 rounded ">
-    <div class="icon w-1/12 ">icon</div>
+  <div class="task-item flex p-4 bg-white mt-2 mb-2 rounded">
+    <div class="icon w-1/12 text-2xl">{{ getRandomEmoji() }}</div>
     <div class="task-item-description w-8/12">
       {{ task.todo }}
     </div>
     <div class="task-item-action w-3/12 justify-end items-center flex">
-      <div class="type rounded text-xs bg-blue-700 text-white  p-2">Personal</div>
-      <div class="status ml-2 rounded-full  h-3 w-3 " :class="{
-        'bg-green-400': task?.completed,
-        'bg-orange-400': !task?.completed,
-      }"></div>
+      <div class="type rounded text-xs bg-blue-700 text-white p-2">Personal</div>
+      <div
+        class="status ml-2 rounded-full h-3 w-3"
+        :class="{
+          'bg-green-400': task?.completed,
+          'bg-orange-400': !task?.completed
+        }"
+      ></div>
     </div>
   </div>
 </template>
@@ -18,6 +21,11 @@
 import type { PropType } from 'vue'
 import type { TaskObject } from '../../../types'
 
+const emojiArray = ['😀', '😃', '😄', '😁', '😆', '😅', '😂', '🤣', '🥲']
+const getRandomEmoji = () => {
+  const emojy = emojiArray[Math.floor(Math.random() * emojiArray.length)]
+  return emojy
+}
 defineProps({
   task: {
     type: Object as PropType<TaskObject>
