fcb7d4cd55bba7e854d3a62113ac0fed981267c1
Mohamed Habib ALOUI <alouimohamedhabib@yahoo.com>
2024-04-21 00:06:10 +0200

    fix props mutation on taskEdit component

--- src/components/Tasks/TaskEdit.vue
+++ src/components/Tasks/TaskEdit.vue
@@ -16,8 +16,19 @@
         />
       </div>
       <div class="mb-6">
-        <label for="" class="block mb-2 text-sm font-medium text-gray-900">Done or not</label>
-        <input type="checkbox" :checked="task.completed" v-model="todoCopy.completed" />
+        <label class="inline-flex items-center cursor-pointer">
+          <span class="mr-2">Status</span>
+          <input
+            type="checkbox"
+            :checked="task.completed"
+            v-model="todoCopy.completed"
+            class="sr-only peer"
+          />
+          <div
+            class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"
+          ></div>
+          <!-- <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Toggle me</span> -->
+        </label>
       </div>
       <button
         type="submit"
